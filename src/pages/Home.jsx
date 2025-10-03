8bb60e5f02ac5d79c1ec836a89494f6cbbdd7fc2
brunoc88 <brunocerutti88@gmail.com>
2025-10-03 10:15:25 -0300

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
+    const navigate = useNavigate()
+
+    const goToAbout = () => {
+        navigate('/about')
+    }
     return (
         <div className="home-container">
             <div className="home-card">
@@ -9,17 +15,16 @@ const Home = () => {
                 </div>
                 <div className="home-content">
                     <p>
-                        ¡Hola! Soy Bruno 👋 <br/>
-                        Bienvenido a mi portafolio como desarrollador. <br/>
+                        ¡Hola! Soy Bruno 👋 <br />
+                        Bienvenido a mi portafolio como desarrollador. <br />
                         Aquí quiero compartirte un poco sobre mí, mi recorrido en programación y los proyectos que he estado construyendo.
                     </p>
                     <div className="home-buttons">
-                        <button className="btn">Comenzar</button>
-                        <button className="btn">Salir</button>
+                        <button className="btn" onClick={goToAbout}>Comenzar</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
